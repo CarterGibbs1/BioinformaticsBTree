89b9ff300e483ef946bc097473f31e42da369620
Mesa Greear <mesagreear@u.boisestate.edu>
2022-04-07 01:25:48 -0600

    #1: Improved documentation in BNode and NodeType & Implemented Get/Set methods in BNode

--- src/main/java/cs321/btree/BNode.java
+++ src/main/java/cs321/btree/BNode.java
@@ -8,6 +8,7 @@ public class BNode<E> {
 	//child0 <= key0 <= child1 <= key1 <= child2 ... childn <= keyn <= childn + 1
 	private LinkedList<TreeObject<E>> keys; //objects in this node, also size() = n
 	private LinkedList<BNode<E>> children;  //children in this node
+	
 	private BNode<E> parent;  //parent pointer
 	private NodeType type;    //Either root, interior, or leaf node
 	
@@ -43,8 +44,8 @@ public class BNode<E> {
 	 * Insert the given key into this BNode and insert the given child
 	 * to the right of the inserted key.
 	 * 
-	 * @param key
-	 * @param child
+	 * @param key   TreeObject containing Object to insert
+	 * @param child Child related to key to insert 
 	 */
 	/*
 	 * Example Demonstration:
@@ -60,12 +61,51 @@ public class BNode<E> {
 	public void insert(TreeObject<E> key, BNode<E> child) {
 		
 		//get to the index of the first k less than key
-		//TODO: 'key.equals(keys.get(i))' needs to be something like 'key.compareTo(keys.get(i)) <= 0
+		//TODO: 'key.equals(keys.get(i))' needs to be something like 'key.compareTo(keys.get(i)) <= 0'
 		int i;
 		for(i = ( keys.size() - 1); i >= 0 && key.equals(keys.get(i)); i--){}
+		
+		//add new key and child to lists
 		keys.add(i + 1, key);
-		children.add(i + 2, null);
+		children.add(i + 2, child);
 		
 		//TODO: write to disk, probably in BTree.java
+	}
+	
+	/**
+	 * Get the number of objects (n) in this BNode represented by
+	 * keys.size().
+	 * 
+	 * @return Number of objects in this BNode
+	 */
+	public int getN() {
+		return keys.size();
+	}
+	
+	/**
+	 * Get the type this BNode is, i.e. ROOT, INTERIOR, or LEAF.
+	 * 
+	 * @return this BNode's type
+	 */
+	public NodeType getType() {
+		return type;
+	}
+	
+	/**
+	 * Set the type for this BNode, i.e. ROOT, INTERIOR, or LEAF.
+	 * 
+	 * @param type new type for this BNode
+	 */
+	public void setType(NodeType type) {
+		this.type = type;
+	}
+	
+	/**
+	 * Indicates whether or not this BNode is full (n = 2t - 1)
+	 * 
+	 * @return true is full, false otherwise
+	 */
+	public boolean isFull() {
+		return ((2 * DEGREE) - 1) == keys.size();
 	}
 }
