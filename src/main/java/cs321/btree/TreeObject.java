c54e5ceb7a9032eb935e5ea11e6b868a159d8590
Aaron Goin <aarongoin1990@gmail.com>
2022-04-13 11:54:46 -0600

    #8: New TreeObject refactored, lowercase letters

--- src/main/java/cs321/btree/TreeObject.java
+++ src/main/java/cs321/btree/TreeObject.java
@@ -12,36 +12,45 @@ package cs321.btree;
  * @param <E> Generic Type for this TreeObject to hold
  */
 public class TreeObject<E> {
-    // valid treeObjects: "A", "T", "C", "G"
+    // valid treeObjects:               "a", "t", "c", "g" (lowercase)
     // corresponding 2-bit binary value: 00, 11, 01, 10
-    // placeholder values: 99(for 00) 98(for 01)
-    long treeObjectKey;
+    private long treeObjectKey;
     private int frequency;
 
     /**
      * Constructor: Creates a TreeObject with the specified key. Also sets the value
      * of b depending on the possibleKey. The key values in treeObjectKey must all
-     * consist of "A", "C", "G", or "T" to convert to a usable long value.
-     * Placeholder values are used initially for easier access in other methods.
+     * consist of "a", "c", "g", or "t" to convert to a usable long value.
      *
      * @param treeObjectKey the key of the object
-     * @param frequency   size of each, individual sequence in possibleKey
+     * @param frequency     size of each, individual sequence in possibleKey
      */
     public TreeObject(E treeObjectKey, int frequency) {
         String s = (String) treeObjectKey;
-        String str = s.replaceAll("A", "99").replaceAll("T", "11").replaceAll("C", "98").replaceAll("G", "10");
-        this.treeObjectKey = Long.parseLong(str);
+        String b = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                b += "00";
+            }
+            if (s.charAt(i) == 'c') {
+                b += "01";
+            }
+            if (s.charAt(i) == 'g') {
+                b += "10";
+            }
+            if (s.charAt(i) == 't') {
+                b += "11";
+            }
+        }
+        this.treeObjectKey = Long.parseLong(b, 2);
         this.frequency = frequency;
     }
 
     /**
-     * Returns the long value of the treeObjectKey after getting rid of placeholder
-     * values.
-     *
      * @return the key of the TreeObject
      */
     public long getKey() {
-        return Long.parseLong(withZeros());
+        return treeObjectKey;
     }
 
     /**
@@ -50,8 +59,7 @@ public class TreeObject<E> {
      * @return a String value with the correct binary values
      */
     public String withZeros() {
-        String s = Long.toString(treeObjectKey);
-        return s.replaceAll("99", "00").replaceAll("98", "01");
+        return Long.toBinaryString(treeObjectKey);
     }
 
     /**
@@ -70,10 +78,10 @@ public class TreeObject<E> {
 
     /**
      * Determines if the left TreeObject's b value is larger than the parameter
-     * "right" TreeObject's key value TreeObjects that are being inserted into a BTree
-     * go through this method. If left is equal or less than right, then they go the
-     * same direction. The return value is exact in terms of the difference of the
-     * two long values.
+     * "right" TreeObject's key value TreeObjects that are being inserted into a
+     * BTree go through this method. If left is equal or less than right, then they
+     * go the same direction. The return value is exact in terms of the difference
+     * of the two long values.
      *
      * @param right the TreeObject being compared to the current TreeObject
      * @return negative number if left is less than right, positive number if left
@@ -93,7 +101,7 @@ public class TreeObject<E> {
         String[] kTSA = stringSequences();
         int j = 0;
         for (String s : kTSA) {
-            String str = s.replaceAll("A", "00").replaceAll("T", "11").replaceAll("C", "01").replaceAll("G", "10");
+            String str = s.replaceAll("a", "00").replaceAll("t", "11").replaceAll("c", "01").replaceAll("g", "10");
             kTSA[j] = str;
             j++;
         }
@@ -106,8 +114,7 @@ public class TreeObject<E> {
      * @return a String array with all String sequences
      */
     public String[] stringSequences() {
-        String tOk = Long.toString(treeObjectKey);
-        tOk = tOk.replaceAll("99", "A").replaceAll("98", "C").replaceAll("10", "G").replaceAll("11", "T");
+        String tOk = stringWithLetters();
         String[] s = new String[tOk.length() - frequency + 1];
         for (int i = 0; i <= tOk.length() - frequency; i++) {
             String t = "";
@@ -124,15 +131,37 @@ public class TreeObject<E> {
     }
 
     /**
-     * A String form of the TreeObject's key and frequency, formatted for dump files.
+     * @return the treeObjectKey in lettered form
+     */
+    private String stringWithLetters() {
+        String s = withZeros();
+        String b = "";
+        for (int i = 0; i < s.length() - 1; i += 2) {
+            String str = s.substring(i, i + 2);
+            if (str.equals("00")) {
+                b += "a";
+            }
+            if (str.equals("01")) {
+                b += "c";
+            }
+            if (str.equals("10")) {
+                b += "g";
+            }
+            if (str.equals("11")) {
+                b += "t";
+            }
+        }
+        return b;
+    }
+
+    /**
+     * A String form of the TreeObject's "lettered" key and frequency, formatted for dump
+     * files.
      *
      * @return a String representation of a TreeObject
      */
     public String toString() {
-        String s = Long.toString(treeObjectKey).replaceAll("99", "A").replaceAll("11", "T").replaceAll("98", "C")
-                .replaceAll("10", "G");
-        s += ": " + frequency;
-        return s;
+        return withZeros() + ": " + frequency;
     }
 
 }
