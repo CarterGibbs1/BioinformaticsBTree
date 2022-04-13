2caf4617c2fd6db82482010622265971921ec26a
Aaron Goin <aarongoin1990@gmail.com>
2022-04-12 19:54:02 -0600

    #8: New TreeObject after Tuesday

--- src/main/java/cs321/btree/TreeObject.java
+++ src/main/java/cs321/btree/TreeObject.java
@@ -1,329 +1,131 @@
 package cs321.btree;
 
+
 /**
- * An object that is to be stored in a BTree.
- * Specific placement in the BTree is determined by previous TreeObjects and key values.
- * A notable method is compare(), which determines which object is larger in key value.
+ * An object that is to be stored in a BTree. Specific placement in the BTree is
+ * determined by previous TreeObjects and key values. A notable method is
+ * compare(), which determines which object is larger in key value.
  *
- * @authors Aaron Goin, Mesa Greear
+ * @author Aaron Goin
  * @version Spring 2022
  *
  * @param <E> Generic Type for this TreeObject to hold
  */
 public class TreeObject<E> {
     // valid treeObjects: "A", "T", "C", "G"
-    // corresponding byte value: 00, 11, 01, 10
-    private E treeObjectKey;
-    private byte b = 0;
-    private String stringOfBytes;
+    // corresponding 2-bit binary value: 00, 11, 01, 10
+    // placeholder values: 99(for 00) 98(for 01)
+    long treeObjectKey;
+    private int frequency;
 
     /**
-     * Constructor: Creates a TreeObject with the specified key.
-     * Also sets the value of b depending on the possibleKey.
-     * The key must be "A", "C", "G", or "T" for treeObjectKey to not be null.
-     * -1 is used to equal b if the possibleKey is invalid
+     * Constructor: Creates a TreeObject with the specified key. Also sets the value
+     * of b depending on the possibleKey. The key values in treeObjectKey must all
+     * consist of "A", "C", "G", or "T" to convert to a usable long value.
+     * Placeholder values are used initially for easier access in other methods.
      *
-     * @param possibleKey a key that may get assigned to the object, if all chars are listed above
+     * @param treeObjectKey the key of the object
+     * @param frequency   size of each, individual sequence in possibleKey
      */
-    public TreeObject(E possibleKey) {
-        char[] c = keyToCharArray(possibleKey);
-        StringBuilder toStringChars = new StringBuilder();
-        for (int i = 0; i < c.length; i++) {
-            byte index = toBinary(c[i]);
-            if (index == -1) {
-                b = -1;
-                break;
-            }
-            toStringChars.append(charToBinary(c[i]));
-            b += index;
-        }
-        if (b == -1) {
-            treeObjectKey = null;
-            stringOfBytes = null;
-        } else {
-            treeObjectKey = possibleKey;
-            stringOfBytes = toStringChars.toString();
-        }
+    public TreeObject(E treeObjectKey, int frequency) {
+        String s = (String) treeObjectKey;
+        String str = s.replaceAll("A", "99").replaceAll("T", "11").replaceAll("C", "98").replaceAll("G", "10");
+        this.treeObjectKey = Long.parseLong(str);
+        this.frequency = frequency;
     }
 
     /**
-     * Used to simplify processes where a char array is needed to find an overall byte value or toString.
+     * Returns the long value of the treeObjectKey after getting rid of placeholder
+     * values.
      *
-     * @param objectKey an element value that may get converted to a treeObjectKey
-     * @return a char array of the objectKey
-     */
-    private char[] keyToCharArray(E objectKey) {
-        String s = (String) objectKey;
-        return s.toCharArray();
-    }
-
-    /**
-     * Converts a char in a treeObjectKey to a 2-bit binary number. This helps
-     * preserve memory. Used in constructor.
-     *
-     * @param possibleKeyChar a char in a key that is being evaluated
-     * @return byte that represents the key, or -1 if treeObjectKey isn't valid
-     */
-    private byte toBinary(char possibleKeyChar) {
-        byte b;
-        if (possibleKeyChar == 'A') {
-            b = 0b00;
-        } else if (possibleKeyChar == 'T') {
-            b = 0b11;
-        } else if (possibleKeyChar == 'C') {
-            b = 0b01;
-        } else if (possibleKeyChar == 'G') {
-            b = 0b10;
-        } else {
-            b = -1;
-        }
-        return b;
-    }
-
-    /**
-     * Converts a char to a 2-bit binary number if it is a valid TreeObject
-     *
-     * @param c a character that can get converted to its respective 2-bit binary number
-     * @return a String of the corresponding binary number of the char
-     */
-    private String charToBinary(char c) {
-        String byteS;
-        if (c == 'A') {
-            byteS = "00";
-        } else if (c == 'T') {
-            byteS = "11";
-        } else if (c == 'C') {
-            byteS = "01";
-        } else if (c == 'G') {
-            byteS = "10";
-        } else {
-            byteS = "-1";
-        }
-        return byteS;
-    }
-
-    /**
      * @return the key of the TreeObject
      */
-    public E getElement() {
-        return treeObjectKey;
+    public long getKey() {
+        return Long.parseLong(withZeros());
     }
 
     /**
-     * @return b, the byte representation of the key
+     * Replaces all placeholder values with their corresponding values
+     *
+     * @return a String value with the correct binary values
      */
-    public byte getB() {
-        return b;
+    public String withZeros() {
+        String s = Long.toString(treeObjectKey);
+        return s.replaceAll("99", "00").replaceAll("98", "01");
     }
 
     /**
-     * Determines if the left TreeObject's b value is larger than the parameter "right" TreeObject's b value
-     * TreeObjects that are being inserted into a BTree go through this method.
-     * If left is equal or less than right, then they go the same direction.
-     * The return value is exact in terms of the difference of the two byte values.
+     * @return the frequency of the TreeObject
+     */
+    public int getFrequency() {
+        return frequency;
+    }
+
+    /**
+     * Determines if the left TreeObject's b value is larger than the parameter
+     * "right" TreeObject's key value TreeObjects that are being inserted into a BTree
+     * go through this method. If left is equal or less than right, then they go the
+     * same direction. The return value is exact in terms of the difference of the
+     * two long values.
      *
      * @param right the TreeObject being compared to the current TreeObject
-     * @return negative number if left is less than right, positive number if left is larger than right,
-     * 0 if equal
+     * @return negative number if left is less than right, positive number if left
+     *         is larger than right, 0 if equal
      */
     public int compare(TreeObject<E> right) {
-        byte left = b;// for easier code explanation in documentation
-        return Byte.compare(left, right.getB());
+        long left = this.getKey();// for easier code explanation in documentation
+        return Long.compare(left, right.getKey());
     }
 
     /**
-     * A String form of the TreeObject. It's simply the key in raw and binary form
+     * Creates a String array that holds all binary sequences of length frequency
+     *
+     * @return a String array with all binary sequences
+     */
+    public String[] binarySequences() {
+        String[] kTSA = stringSequences();
+        int j = 0;
+        for (String s : kTSA) {
+            String str = s.replaceAll("A", "00").replaceAll("T", "11").replaceAll("C", "01").replaceAll("G", "10");
+            kTSA[j] = str;
+            j++;
+        }
+        return kTSA;
+    }
+
+    /**
+     * Creates a String array that holds all String sequences of length frequency
+     *
+     * @return a String array with all String sequences
+     */
+    public String[] stringSequences() {
+        String tOk = Long.toString(treeObjectKey);
+        tOk = tOk.replaceAll("99", "A").replaceAll("98", "C").replaceAll("10", "G").replaceAll("11", "T");
+        String[] s = new String[tOk.length() - frequency + 1];
+        for (int i = 0; i <= tOk.length() - frequency; i++) {
+            String t = "";
+            int freqCount = 0;
+            int j = i;
+            while (freqCount < frequency) {
+                t += tOk.charAt(j);
+                freqCount++;
+                j++;
+            }
+            s[i] = t;
+        }
+        return s;
+    }
+
+    /**
+     * A String form of the TreeObject's key and frequency, formatted for dump files.
      *
      * @return a String representation of a TreeObject
      */
     public String toString() {
-        return stringOfBytes;
+        String s = Long.toString(treeObjectKey).replaceAll("99", "A").replaceAll("11", "T").replaceAll("98", "C")
+                .replaceAll("10", "G");
+        s += ": " + frequency;
+        return s;
     }
+
 }
-/*
- * The following are ideas I have for a new way to implement this class if needed, as I read the frequency will
- * need to be something this object knows about.
- */
-//
-//
-///**
-// * An object that is to be stored in a BTree. Specific placement in the BTree is
-// * determined by previous TreeObjects and key values. A notable method is
-// * compare(), which determines which object is larger in key value.
-// *
-// * @author Aaron Goin
-// * @version Spring 2022
-// *
-// * @param <E> Generic Type for this TreeObject to hold
-// */
-//public class TreeObjectOldTwo<E> {
-//	// valid treeObjects: "A", "T", "C", "G"
-//	// corresponding byte value: 00, 11, 01, 10
-//	private E treeObjectKey;
-//	private int frequency;
-//
-//	/**
-//	 * Constructor: Creates a TreeObject with the specified key. Also sets the value
-//	 * of b depending on the possibleKey. The key values must all be "A", "C", "G",
-//	 * or "T" for treeObjectKey to not be null. -1 is used to equal b if the
-//	 * possibleKey is invalid
-//	 *
-//	 * @param possibleKey the key of the object
-//	 * @param frequency size of each, individual sequence in possibleKey
-//	 */
-//	public TreeObjectOldTwo(E treeObjectKey, int frequency) {
-//		this.treeObjectKey = treeObjectKey;
-//		this.frequency = frequency;
-//	}
-//
-//	/**
-//	 * @return the key of the TreeObject
-//	 */
-//	public E getElement() {
-//		return treeObjectKey;
-//	}
-//
-//	/**
-//	 * Determines if the left TreeObject's b value is larger than the parameter
-//	 * "right" TreeObject's b value TreeObjects that are being inserted into a BTree
-//	 * go through this method. If left is equal or less than right, then they go the
-//	 * same direction. The return value is exact in terms of the difference of the
-//	 * two byte values.
-//	 *
-//	 * @param right the TreeObject being compared to the current TreeObject
-//	 * @return negative number if left is less than right, positive number if left
-//	 *         is larger than right, 0 if equal
-//	 */
-//	public int compare(TreeObjectOldTwo<E> right) {
-//		byte left = this.getBValue();// for easier code explanation in documentation
-//		return Byte.compare(left, right.getBValue());
-//	}
-//
-//	/**
-//	 * @return b, the byte representation of the key
-//	 */
-//	private byte getBValue() {
-//		String s = (String) treeObjectKey;
-//		char[] c = s.toCharArray();
-//		byte b = 0;
-//		for (int i = 0; i < c.length; i++) {
-//			byte index = toBinary(c[i]);
-//			if (index == -1) {
-//				return -1;
-//			}
-//			b += index;
-//		}
-//		return b;
-//	}
-//
-//	/**
-//	 * Converts a char in a treeObjectKey to a 2-bit binary number. This helps
-//	 * preserve memory. Used in constructor.
-//	 *
-//	 * @param possibleKeyChar a char in a key that is being evaluated
-//	 * @return byte that represents the key, or -1 if treeObjectKey isn't valid
-//	 */
-//	private byte toBinary(char possibleKeyChar) {
-//		byte b;
-//		if (possibleKeyChar == 'A') {
-//			b = 0b00;
-//		} else if (possibleKeyChar == 'T') {
-//			b = 0b11;
-//		} else if (possibleKeyChar == 'C') {
-//			b = 0b01;
-//		} else if (possibleKeyChar == 'G') {
-//			b = 0b10;
-//		} else {
-//			b = -1;
-//		}
-//		return b;
-//	}
-//
-////	public String[] getEntireB() {// all possible sequences
-////		String l[] = new String[howManySequences()];
-////		String b = binarySequences();
-////		String s[] = keyToStringArray();
-////		int i = 0;
-////		int j = 0;
-////		for(String str : s) {
-////			char ch[] = str.toCharArray();
-////			String string = "";
-////			j = frequency * i;
-////			for (char c : ch) {
-////				string += b.charAt(j);
-////				j++;
-////				string += b.charAt(j);
-////				j++;
-////			}
-////			l[i] = string;
-////			i++;
-////		}
-////		return l;
-////	}
-//
-////	private int howManySequences() {
-////		int start = treeObjectKey.toString().length() + 1;
-////		int x = -1 * (frequency);
-////		return x + start;
-////	}
-//
-//	public String[] binarySequences() {
-//		String[] kTSA = keyToStringArray();
-//		int j = 0;
-//		for (String s : kTSA) {
-//			String str = s.replaceAll("A", "00").replaceAll("T", "11")
-//			.replaceAll("C", "01").replaceAll("G", "10");
-//			kTSA[j] = str;
-//			j++;
-//		}
-//		return kTSA;
-//	}
-//
-//	/**
-//	 * Used to simplify processes where a char array is needed to find an overall byte value or toString.
-//	 *
-//	 * @return a char array of the objectKey
-//	 */
-//	public String[] keyToStringArray() {
-//		String tOK = this.toString();
-//		String[] s = new String[tOK.length() - frequency + 1];
-//		for (int i = 0; i <= tOK.length() - frequency; i++) {
-//			String t = "";
-//			int freqCount = 0;
-//			int j = i;
-//			while (freqCount < frequency) {
-//				t += tOK.charAt(j);
-//				freqCount++;
-//				j++;
-//			}
-//			s[i] = t;
-//		}
-//		return s;
-//	}
-//
-////	public byte getEntireB() {
-////		String s = binarySequences();
-////		String[] t = keyToStringArray();
-////		byte a[] = new byte[s.length()];
-////		for (int i = 0; i < s.length(); i++) {
-////
-////			for (int j = 0; j < frequency; j++) {
-////
-////			}
-////			a[i] = Byte.parseByte();
-////		}
-////		ByteBuffer b = ByteBuffer.wrap(s);
-////		return b.get();
-////	}
-//
-//
-//	/**
-//	 * A String form of the TreeObject. It's simply the key in raw and binary form
-//	 *
-//	 * @return a String representation of a TreeObject
-//	 */
-//	public String toString() {
-//		return (String) treeObjectKey;
-//	}
-//
-//}
