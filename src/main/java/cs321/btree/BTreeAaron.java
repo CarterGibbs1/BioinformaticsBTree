e16ee7ecefe53fcce6dbd0b5f19bfdd5ede1c982
Mesa Greear <mesagreear@u.boisestate.edu>
2022-04-13 14:58:51 -0600

    #16: Alternate implementation of insert

--- src/main/java/cs321/btree/BTreeAaron.java
+++ src/main/java/cs321/btree/BTreeAaron.java
@@ -12,6 +12,7 @@ package cs321.btree;
  */
 public class BTreeAaron<E> {
     private BNode<E> root;
+    private TestBNode<E> testRoot;
     private final int degree;
     private int numNodes;
 
@@ -42,5 +43,37 @@ public class BTreeAaron<E> {
         } else {
             root.insert(toInsert, r);
         }
+    }
+    
+    /**
+     * Inserts an element into the list. sections are labeled BInsert and
+     * BInsertNotFull to show which sections somewhat match the pseudo code.
+     * 
+     * @param element Generic to insert into BTree
+     */
+    public void insert(E element) {
+    	TreeObject<E> key = new TreeObject<E>(element, 1);
+    	TestBNode<E> r = testRoot;
+    	
+    	//====BInsert==================
+    	//if root is full, split it and reassign to the new root
+    	if(r.isFull(degree)) {
+    		testRoot = r.split(); //TODO: write to disk could be dealt in BNode or in BTree
+    	}
+    	
+    	//====BInsertNotFull==========
+    	//starting at root, search down BTree to find correct leaf node to insert into
+    	TestBNode<E> currentNode = testRoot;
+    	while(!currentNode.isLeaf()) {
+    		currentNode = currentNode.getSubtree(key);
+    		
+    		//if the current node is full, split it and assign currentNode to parent
+    		if(currentNode.isFull(degree)) {
+    			currentNode = currentNode.split(); //TODO: write to disk could be dealt in BNode or in BTree
+    		}
+    	}
+    	
+    	//once at leaf node, insert key
+    	currentNode.insert(key);
     }
 }
