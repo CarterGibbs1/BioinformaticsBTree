9bf1c9c563715d3ecd8f2e1ca4cc3dd955ecf329
Aaron Goin <aarongoin1990@gmail.com>
2022-04-14 09:05:30 -0600

    #16: Modified methods in accordance with "test 1" handout

--- src/main/java/cs321/btree/BTreeAaron.java
+++ src/main/java/cs321/btree/BTreeAaron.java
@@ -1,18 +1,18 @@
 package cs321.btree;
 
+
 /**
  * Used to create BTree objects that hold Generic Type objects.
  * Notable method is insert() which is important in structuring the
  * BTree. Aaron's version, using Carter's code as baseline.
  *
- * @author  Aaron Goin, Mesa Greear
+ * @author  Aaron Goin, Mesa Greear, Carter Gibbs
  * @version Spring 2022
  *
  * @param <E> Generic Type for this BNode to hold
  */
 public class BTreeAaron<E> {
-    private BNode<E> root;
-    private TestBNode<E> testRoot;
+    private TestBNode<E> root;
     private final int degree;
     private int numNodes;
 
@@ -22,40 +22,69 @@ public class BTreeAaron<E> {
      * @param degree the degree of the BTree (minimum number of children for internal nodes other than the root)
      */
     public BTreeAaron(int degree) {
-        root = new BNode<E>(null);
+        root = new TestBNode<E>(null);
         numNodes = 0;
         this.degree = degree;
     }
-    
+
     /**
      * Inserts an element into the list. sections are labeled BInsert and
      * BInsertNotFull to show which sections somewhat match the pseudo code.
-     * 
+     *
      * @param element Generic to insert into BTree
      */
     public void insert(E element) {
-    	TreeObject<E> key = new TreeObject<E>(element, 1);
-    	TestBNode<E> r = testRoot;
-    	
-    	//====BInsert==================
-    	//if root is full, split it and reassign to the new root
-    	if(r.isFull(degree)) {
-    		testRoot = r.split(); //TODO: write to disk could be dealt in BNode or in BTree
-    	}
-    	
-    	//====BInsertNotFull==========
-    	//starting at root, search down BTree to find correct leaf node to insert into
-    	TestBNode<E> currentNode = testRoot;
-    	while(!currentNode.isLeaf()) {
-    		currentNode = currentNode.getSubtree(key);
-    		
-    		//if the current node is full, split it and assign currentNode to parent
-    		if(currentNode.isFull(degree)) {
-    			currentNode = currentNode.split(); //TODO: write to disk could be dealt in BNode or in BTree
-    		}
-    	}
-    	
-    	//once at leaf node, insert key
-    	currentNode.insert(key);
+        TreeObject<E> k = new TreeObject<E>(element, 1);
+        TestBNode<E> r = root;// step 1 of pseudo code
+
+        //====BInsert==================
+        //if root is full, split it and reassign to the new root
+        if(r.isFull(degree)) {//step 2
+            TestBNode<E> s = null; // part of step 3, need to allocate
+            root = s; // step 4, step 5 and 6 may already be covered
+            s.insert(r.rootKey(), r);// step 7?
+            s = r.split();//step 8 //TODO: write to disk could be dealt in BNode or in BTree
+            insertNonFull(s, k);
+            //increment numNodes
+            numNodes++;
+        }
+        else  {
+            insertNonFull(r, k);
+        }
+
     }
+
+    /**
+     * Inserts key into nonfull node
+     *
+     * @param x the node that isn't full
+     * @param key the key that is being inserted
+     */
+    private void insertNonFull(TestBNode<E> x, TreeObject<E> key) {
+        //starting at root, search down BTree to find correct leaf node to insert into
+        int i = x.getN();//step 1
+        while(!x.isLeaf()) {
+            x = x.getSubtree(key);
+
+            //if the current node is full, split it and assign currentNode to parent
+            if(x.isFull(degree)) {
+                x = x.split(); //TODO: write to disk could be dealt in BNode or in BTree
+            }
+        }
+        //once at leaf node, insert key
+        x.insert(key);
+    }
+
+    public TestBNode<E> getRoot() {
+        return root;
+    }
+
+    public int getNumNodes() {
+        return numNodes;
+    }
+
+//    public String toString() {
+//
+//    }
 }
+
