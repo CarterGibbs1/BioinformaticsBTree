aeaac185150fcea93577a2f73783564d52a4eb24
Aaron Goin <aarongoin1990@gmail.com>
2022-04-15 09:03:18 -0600

    #16: Watered down BNode class for BTree imp

--- src/main/java/cs321/btree/TestBNodeNoE.java
+++ src/main/java/cs321/btree/TestBNodeNoE.java
@@ -3,30 +3,31 @@ package cs321.btree;
 import java.util.LinkedList;
 
 /**
- * Similar to BNode.java, but references to other BNodes are now
- * pointers stored in memory instead of addresses to a RAF. Useful for
- * testing that BTree's are working.
+ * Similar to BNode.java, but references to other BNodes are now pointers stored
+ * in memory instead of addresses to a RAF. Useful for testing that BTree's are
+ * working.
  *
- * @author  Mesa Greear
+ * @author Mesa Greear, Aaron Goin
  * @version Spring 2022
  *
  */
 public class TestBNodeNoE {
 
-    //child0 <= key0 <= child1 <= key1 <= child2 ... childn <= keyn <= childn + 1
-    private LinkedList<TreeObjectNoE> keys; //objects/keys in this node, also size() = n
-    private LinkedList<TestBNodeNoE> children;  //children in this node
+    // child0 <= key0 <= child1 <= key1 <= child2 ... childn <= keyn <= childn + 1
+    private LinkedList<TreeObjectNoE> keys; // objects/keys in this node, also size() = n
+    private LinkedList<String> children; // children in this node
+    private String parent; // parent pointer
+    private boolean isLeaf;
+    private long location;// in Random Access File
 
-    private TestBNodeNoE parent;  //parent pointer
-
-    //=================================================================================================================
-    //                                               CONSTRUCTORS
-    //=================================================================================================================
+    // =================================================================================================================
+    // CONSTRUCTORS
+    // =================================================================================================================
 
     /**
-     * Constructor: Create BNode with one key initialKey, a parent pointer
-     * parent, two children leftChild and rightChild, and the degree (t) of
-     * this BNode. BNode type is determined automatically by parameters.
+     * Constructor: Create BNode with one key initialKey, a parent pointer parent,
+     * two children leftChild and rightChild, and the degree (t) of this BNode.
+     * BNode type is determined automatically by parameters.
      *
      * @param initialKey  the initial object in this BNode
      * @param parent     pointer to the parent of this BNode
@@ -37,38 +38,37 @@ public class TestBNodeNoE {
      * Example Demonstration:
      *
      *
-     *      new BNode(?, a, ?, /, \)
+     * new BNode(?, a, ?, /, \)
      *
-     * result     -    a
-     *                / \
+     * result - a / \
      */
-    public TestBNodeNoE(TreeObjectNoE initialKey, TestBNodeNoE parent, TestBNodeNoE leftChild, TestBNodeNoE rightChild) {
-        //initialize instance variables
+    public TestBNodeNoE(TreeObjectNoE initialKey, String parent, String leftChild, String rightChild) {
+        // initialize instance variables
         keys = new LinkedList<TreeObjectNoE>();
-        children = new LinkedList<TestBNodeNoE>();
+        children = new LinkedList<String>();
 
         keys.add(initialKey);
         children.add(leftChild);
         children.add(rightChild);
 
         this.parent = parent;
+        this.isLeaf = true;
+        this.location = -1;// indicate it's not written to disk yet
     }
 
     /**
-     * Constructor: Create LEAF BNode with one key initialKey and a
-     * parent pointer parent. Degree (t) is determined by the parent
-     * pointer.
+     * Constructor: Create LEAF BNode with one key initialKey and a parent pointer
+     * parent. Degree (t) is determined by the parent pointer.
      *
-     * @param initialKey  the initial object in this BNode
-     * @param parent     pointer to the parent of this BNode
+     * @param initialKey the initial object in this BNode
+     * @param parent    pointer to the parent of this BNode
      */
-    public TestBNodeNoE(TreeObjectNoE initialKey, TestBNodeNoE parent) {
+    public TestBNodeNoE(TreeObjectNoE initialKey, String parent) {
         this(initialKey, parent, null, null);
     }
 
     /**
-     * Constructor: Create ROOT BNode with one key initialKey and the degree
-     * (t).
+     * Constructor: Create ROOT BNode with one key initialKey and the degree (t).
      *
      * @param initialKey the initial object in this BNode
      */
@@ -76,126 +76,133 @@ public class TestBNodeNoE {
         this(initialKey, null, null, null);
     }
 
-    //=================================================================================================================
-    //                                         BTREE FUNCTIONALITY METHODS
-    //=================================================================================================================
+    // =================================================================================================================
+    // BTREE FUNCTIONALITY METHODS
+    // =================================================================================================================
+
+//	/**
+//	 * Insert the given key into this BNode and insert the given child to the right
+//	 * of the inserted key.
+//	 *
+//	 * @param key   TreeObjectNoE containing Object to insert
+//	 * @param child Child related to key to insert
+//	 */
+//	/*
+//	 * Example Demonstration:
+//	 *
+//	 *
+//	 * keys - a b d e f children - # # # # # #
+//	 *
+//	 * insert(c, *)
+//	 *
+//	 * keys - a b c d e f children - # # # * # # #
+//	 */
+//	public void insert(TreeObjectNoE key, String child) {
+//
+//		// get to the index of the first k less than key
+//		int i;
+//		for (i = (keys.size() - 1); i >= 0 && key.compare(keys.get(i)) <= 0; i--) {
+//		}
+//
+//		// add new key and child to lists
+//		keys.add(i + 1, key);
+//		children.add(i + 2, child);
+//
+//		// TODO: write to disk, probably in BTree.java
+//	}
 
     /**
-     * Insert the given key into this BNode and insert the given child
-     * to the right of the inserted key.
+     * Insert the given key into this BNode. Should only be used on LEAF nodes. If a
+     * node y is in the right subtree of a node x, then key[y] > key[x].
      *
-     * @param key   TreeObject containing Object to insert
-     * @param child Child related to key to insert
-     */
-    /*
-     * Example Demonstration:
-     *
-     *
-     * keys     -  a b d e f
-     * children - # # # # # #
-     *
-     *            insert(c, *)
-     *
-     * keys     -  a b c d e f
-     * children - # # # * # # #
-     */
-    public void insert(TreeObjectNoE key, TestBNodeNoE child) {
-
-        //get to the index of the first k less than key
-        int i;
-        for(i = ( keys.size() - 1); i >= 0 && key.compare(keys.get(i)) <= 0; i--){}
-
-        //add new key and child to lists
-        keys.add(i + 1, key);
-        children.add(i + 2, child);
-
-        //TODO: write to disk, probably in BTree.java
-    }
-
-    /**
-     * Insert the given key into this BNode. Should only be used on LEAF
-     * nodes.
-     *
-     * @param key TreeObject containing Object to insert
+     * @param key TreeObjectNoE containing Object to insert
      */
     public void insert(TreeObjectNoE key) {
-        insert(key, null);
+        int i = 0;
+        while (i < keys.size()) {
+            if (key.compare(keys.get(i)) > 0) {
+                i++;
+            } else {// satisfies the condition in javadoc
+                keys.add(i, key);
+            }
+        }
     }
 
     /**
-     * Get the child of this BNode where the given key should be
-     * inserted or would be located. Does NOT insert the key, only
-     * returns the subtree that it belongs to.
+     * Get the child of this BNode where the given key should be inserted or would
+     * be located. Does NOT insert the key, only returns the subtree that it belongs
+     * to.
      *
      * @param key Object to use to locate the appropriate subtree
      *
      * @return subtree (child of this BNode) that key belongs to
      */
-    public TestBNodeNoE getSubtree(TreeObjectNoE key){
+    public String getSubtree(TreeObjectNoE key) {
 
-        //get to the index of the first k less than key
+        // get to the index of the first k less than key
         int i;
-        for(i = ( keys.size() - 1); i >= 0 && key.compare(keys.get(i)) <= 0; i--){}
+        for (i = (keys.size() - 1); i >= 0 && key.compare(keys.get(i)) <= 0; i--) {
+        }
 
         return children.get(i + 1);
     }
 
-    /**
-     * Splits the current BNode into two new BNodes, removing and
-     * inserting the middle object and a pointer to the new right
-     * BNode into the parent. The left BNode (this BNode) will
-     * contain everything to the left of the removed object and
-     * the right BNode (new BNode) will be contain everything to
-     * the right. Can only be run when the list is full.
-     *
-     * @return the parent of the two BNodes
-     */
-    public TestBNodeNoE split() throws IllegalStateException{
-        //==== for better understanding when coming back to look at this method, I'm going to ====
-        //==== create an example split and show how it changes through comments:              ====
-        //Keys     -  a b c d e f g
-        //Pointers - 0 1 2 3 4 5 6 7
+//	/**
+//	 * Splits the current BNode into two new BNodes, removing and
+//	 * inserting the middle object and a pointer to the new right
+//	 * BNode into the parent. The left BNode (this BNode) will
+//	 * contain everything to the left of the removed object and
+//	 * the right BNode (new BNode) will be contain everything to
+//	 * the right. Can only be run when the list is full.
+//	 *
+//	 * @return the parent of the two BNodes
+//	 */
+//	public TestBNodeNoE split() throws IllegalStateException{
+//		//==== for better understanding when coming back to look at this method, I'm going to ====
+//		//==== create an example split and show how it changes through comments:              ====
+//		//Keys     -  a b c d e f g
+//		//Pointers - 0 1 2 3 4 5 6 7
+//
+//		int originalN = keys.size();
+//
+//
+//		//remove key and two pointers right of middle and insert into new BNode 'splitRight':
+//		//Keys     -  a b c d f g    |  e
+//		//Pointers - 0 1 2 3   6 7   | 4 5
+//		TestBNodeNoE splitRight = new TestBNodeNoE(keys.remove(keys.size()/2 + 1), parent, children.remove(children.size()/2), children.remove(children.size()/2 + 1));
+//
+//
+//		//starting just to the right of the middle of this BNode, continuously remove the pointers and keys
+//		//at that position and insert them into splitRight:
+//		//Keys     -  a b c d  |  e f g
+//		//Pointers - 0 1 2 3   | 4 5 6 7
+//		while((keys.size() - 1) != originalN/2) {
+//			splitRight.insert(keys.remove(originalN/2 + 1), children.remove(originalN/2 + 1));
+//		}
+//
+//
+//		//lastly, remove last node (original middle) from this and insert into parent with
+//		//a pointer to splitRight
+//		//Parent   -    .. x x d x x ..
+//		//                    / \
+//		//Keys     -  a b c d  |  e f g
+//		//Pointers - 0 1 2 3   | 4 5 6 7
+//
+//		//if this is the ROOT, create new parent/root to insert into and update types
+//		if(isRoot()) {
+//			TestBNodeNoE newRoot = new TestBNodeNoE(keys.removeLast(), null, this, splitRight);
+//			this.parent = splitRight.parent = newRoot;
+//
+//			return newRoot;
+//		}
+//
+//		parent.insert(keys.removeLast(), splitRight);
+//		return parent;
+//	}
 
-        int originalN = keys.size();
-
-
-        //remove key and two pointers right of middle and insert into new BNode 'splitRight':
-        //Keys     -  a b c d f g    |  e
-        //Pointers - 0 1 2 3   6 7   | 4 5
-        TestBNodeNoE splitRight = new TestBNodeNoE(keys.remove(keys.size()/2 + 1), parent, children.remove(children.size()/2), children.remove(children.size()/2 + 1));
-
-
-        //starting just to the right of the middle of this BNode, continuously remove the pointers and keys
-        //at that position and insert them into splitRight:
-        //Keys     -  a b c d  |  e f g
-        //Pointers - 0 1 2 3   | 4 5 6 7
-        while((keys.size() - 1) != originalN/2) {
-            splitRight.insert(keys.remove(originalN/2 + 1), children.remove(originalN/2 + 1));
-        }
-
-
-        //lastly, remove last node (original middle) from this and insert into parent with
-        //a pointer to splitRight
-        //Parent   -    .. x x d x x ..
-        //                    / \
-        //Keys     -  a b c d  |  e f g
-        //Pointers - 0 1 2 3   | 4 5 6 7
-
-        //if this is the ROOT, create new parent/root to insert into and update types
-        if(isRoot()) {
-            TestBNodeNoE newRoot = new TestBNodeNoE(keys.removeLast(), null, this, splitRight);
-            this.parent = splitRight.parent = newRoot;
-
-            return newRoot;
-        }
-
-        parent.insert(keys.removeLast(), splitRight);
-        return parent;
-    }
-
-    //=================================================================================================================
-    //                                           GET/SET/UTILITY METHODS
-    //=================================================================================================================
+    // =================================================================================================================
+    // GET/SET/UTILITY METHODS
+    // =================================================================================================================
 
     /**
      * Gets the first key for the root. Used and needed for B-Tree insert.
@@ -207,8 +214,7 @@ public class TestBNodeNoE {
     }
 
     /**
-     * Get the number of objects (n) in this BNode represented by
-     * keys.size().
+     * Get the number of objects (n) in this BNode represented by keys.size().
      *
      * @return Number of objects in this BNode
      */
@@ -222,7 +228,14 @@ public class TestBNodeNoE {
      * @return true if leaf, false otherwise
      */
     public boolean isLeaf() {
-        return children.get(0) == null;
+        return isLeaf;
+    }
+
+    /**
+     * @param isLeaf the new indicator of whether something is a leaf or not
+     */
+    public void setLeaf(boolean isLeaf) {
+        this.isLeaf = isLeaf;
     }
 
     /**
@@ -238,7 +251,7 @@ public class TestBNodeNoE {
      * Indicates whether or not this BNode is full (n = 2t - 1)
      *
      *
-     * @param  degree the degree of this BTree
+     * @param degree the degree of this BTree
      *
      * @return true is full, false otherwise
      */
@@ -246,12 +259,26 @@ public class TestBNodeNoE {
         return ((2 * degree) - 1) == keys.size();
     }
 
-    //returns long value for each key in a single String seperated by spaces
+    /**
+     * @return the location of this node in memory
+     */
+    public long getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location where the location of the node is on disk
+     */
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    // returns long value for each key in a single String seperated by spaces
     @Override
     public String toString() {
         StringBuilder retString = new StringBuilder();
-        for(int i = 0; i < keys.size(); i++) {
-            retString.append(keys.get(i).getKey() + " "); //TODO: return letters instead
+        for (int i = 0; i < keys.size(); i++) {
+            retString.append(keys.get(i).getKey() + " "); // TODO: return letters instead
         }
 
         return retString.toString().substring(0, retString.length() - 1);
