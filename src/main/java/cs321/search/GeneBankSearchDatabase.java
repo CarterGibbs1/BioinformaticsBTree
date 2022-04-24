e4e68541ba63f7a01398265c29a9932885413b5b
Aaron Goin <aarongoin1990@gmail.com>
2022-04-24 15:33:42 -0600

    #23: mo strategy w basic test ced out

--- src/main/java/cs321/search/GeneBankSearchDatabase.java
+++ src/main/java/cs321/search/GeneBankSearchDatabase.java
@@ -1,6 +1,8 @@
 package cs321.search;
 
 import com.sun.tools.javac.util.StringUtils;
+import cs321.btree.BTree;//delete
+import cs321.btree.TreeObject;
 import cs321.common.ParseArgumentException;
 
 import java.io.File;
@@ -22,44 +24,31 @@ public class GeneBankSearchDatabase
         Connection connection = null;
         try
         {
+
+            /*****************************************************************************/
             GeneBankSearchDatabaseArguments a = parseArgs(args);
             // create a database connection
             connection = DriverManager.getConnection(a.getPathToDatabase());
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-
-            /* Below are things we'll have to import into the CreateBTree class */
-            /*****************************************************************************/
-//
-//            statement.executeUpdate("drop table if exists btree");
-//            statement.executeUpdate("create table btree (name dnaseq, int freq)");
-            /* For all numNodes in btree
-            // instead of this, do an in order traversal
-             * for (int i = 1; i <= numNodes; i++) {// instead of this, do an in order traversal
-             *    BNode current = btree.getNodeAtIndex(i);//from parameter
-             *    String dnaseq = current.getStringKey();
-             *    int freq = current.getFrequency();
-             *    statement.executeUpdate("insert into btree values('dnaseq', freq)");
-             * }
-             */
-            /*****************************************************************************/
-
-            // below statements are needed here
-
-            /*****************************************************************************/
             // search logic, similar to searchbtree, start at query file, use database to search for dna seq
             File q = new File(a.getQueryPathway());
             Scanner qScan = new Scanner(q);
             while (qScan.hasNextLine()) {
                 String qCurr = qScan.nextLine().toLowerCase();
-                System.out.println(qCurr + ": ");
-                int count = 0;
-                ResultSet rs = statement.executeQuery("select * from btree where dnaseq = " + qCurr + ";");// seq col and seq row equal to qCurr
-                int currFreq = rs.getInt("freq");
+                System.out.print(qCurr + ": ");
+                ResultSet rs = statement.executeQuery("select * from btree where dnaseq = '" + qCurr + "';");
+                int currFreq;
+                if (!rs.isClosed()) {
+                    currFreq = rs.getInt("freq");
+                } else {
+                    currFreq = 0;
+                }
                 System.out.println(currFreq);
+                rs.close();
             }
-                qScan.close();
+            qScan.close();
             /*****************************************************************************/
 
         }
@@ -89,20 +78,82 @@ public class GeneBankSearchDatabase
             System.out.println("Usage: java GeneBankSearchDatabase <path_to_SQLite_database> <query_file> [<debug_level>]");
             throw new ParseArgumentException("Incorrect number of arguments");
         }
-        File f = new File(args[0]);// should be String of actual db file in the directory as this point
-        if (!f.exists() || !f.isFile()) {// check extension
+        String dbName = args[0];
+        if (!dbName.contains(".db")) {
             throw new ParseArgumentException("Database does not exist");
         }
         File fTwo = new File(args[1]);// should be existing query file in directory
-        if (!fTwo.exists() || !fTwo.isFile()) {//check extension
+        if (!fTwo.exists() || !fTwo.isFile() || !args[1].contains("query")) {//check extension
             throw new ParseArgumentException("Query file does not exist or is not in pwd.");
         }
-        int d = Integer.parseInt(args[2]);
-        if (d != 0) {// only having one option for right now: results on output stream
-            //the results will mirror GeneBankSearchBTree, where the results will be printed on console
-            throw new ParseArgumentException("Debug level is not valid.");
+        int d = 0;
+        if (args.length == 3) {
+            Integer.parseInt(args[2]);
+            if (d != 0) {// only having one option for right now: results on output stream
+                //the results will mirror GeneBankSearchBTree, where the results will be printed on console
+                throw new ParseArgumentException("Debug level is not valid.");
+            }
         }
         return new GeneBankSearchDatabaseArguments(args[0], args[1], d);
     }
 }
 
+/***/
+//Below is some of the implementation needed to create a database in createbtree
+/***/
+//IMPORTANT: BOTH CREATEBTREE AND SEARCHDATABASE NEED THE FOLLOWING COMMAND IN THE DIR IN TERMINAL BEFORE RUNNING PROGRAM:
+// jar xf sqlite-jdbc-3.36.0.3.jar
+
+//public static void main(String[] args) throws Exception//pass in a btree probs as arg
+//{
+//    Connection connection = null;
+//    try
+//    {
+//
+//
+//        /*Test for a driver*/
+//        /*********/
+//        // create a database connection
+//        connection = DriverManager.getConnection("jdbc:sqlite:btree.db");
+//        Statement statement = connection.createStatement();
+//        statement.setQueryTimeout(30);  // set timeout to 30 sec.
+//        statement.executeUpdate("drop table if exists btree;");
+//        statement.executeUpdate("create table btree (dnaseq varchar(255), freq int);");
+//
+//        DON'T NEED THIS PART, AS WE'LL ALREADY HAVE A BTREE CREATED HERE
+//        BTreeAaron b = new BTreeAaron(2, 1, null, "test.txt");
+//        TestBNodeNoE root = new TestBNodeNoE(1);// important to start at address 1
+//        b.setRoot(root);
+//        b.insert(new TreeObjectNoE("a", 1));
+//        END OF BTREE CREATED FOR TESTING
+
+//        //in order traversal here,
+//
+//        //below is a simple insert, don't need this exactly, but will following syntax for each insert
+//        TestBNodeNoE c = b.getNodeAtIndex(1);
+//        String dnaseq = c.getKeys()[1].getStringKey();
+//        int freq = c.getKeys()[1].getFrequency();
+//        statement.executeUpdate("insert into btree (dnaseq, freq) values (\'" + dnaseq + "\', " + freq + ");");
+//
+//    }
+//    catch(SQLException e)
+//    {
+//        // if the error message is "out of memory",
+//        // it probably means no database file is found
+//        System.err.println(e.getMessage());
+//    }
+//    finally
+//    {
+//        try
+//        {
+//            if(connection != null)
+//                connection.close();
+//        }
+//        catch(SQLException e)
+//        {
+//            // connection close failed.
+//            System.err.println(e.getMessage());
+//        }
+//    }
+//}// end of main
+//}
