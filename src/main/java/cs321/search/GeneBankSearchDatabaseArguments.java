eb30e35f7238f88a18287211b9c6a2f53104439c
Aaron Goin <aarongoin1990@gmail.com>
2022-04-24 10:04:20 -0600

    #23: Basic imp of args for database

--- src/main/java/cs321/search/GeneBankSearchDatabaseArguments.java
+++ src/main/java/cs321/search/GeneBankSearchDatabaseArguments.java
@@ -1,6 +1,35 @@
 package cs321.search;
 
-public class GeneBankSearchDatabaseArguments
-{
+public class GeneBankSearchDatabaseArguments {
+    private final String pathToDatabase;
+    private final String queryPathway;
+    private final int debugLevel;
 
+    public GeneBankSearchDatabaseArguments(String pathToDatabase, String queryPathway, int debugLevel) {
+        this.pathToDatabase = pathToDatabase;
+        this.queryPathway = queryPathway;
+        this.debugLevel = debugLevel;
+    }
+
+    @Override
+    public String toString() {
+        //this method was generated using an IDE
+        return "GeneBankCreateBTreeArguments{" +
+                "pathToDatabase=" + pathToDatabase +
+                ", queryPathway=" + queryPathway +
+                ", debugLevel='" + debugLevel +
+                '}';
+    }
+
+    public String getPathToDatabase() {
+        return pathToDatabase;
+    }
+
+    public String getQueryPathway() {
+        return queryPathway;
+    }
+
+    public int debugLevel() {
+        return debugLevel;
+    }
 }
